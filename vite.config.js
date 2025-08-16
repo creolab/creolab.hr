1896fb8587d0efb6af2716bf8836c6c2be2e21a3
Boris Strahija <bstrahija@gmail.com>
2025-08-16 12:17:17 +0200

    Add favicons

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/site.css',
-                'resources/js/site.js',
-            ],
+            input: ["resources/css/site.css", "resources/js/site.js"],
             refresh: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/icons/*",
+                    dest: "icons",
+                },
+            ],
         }),
     ],
 });
